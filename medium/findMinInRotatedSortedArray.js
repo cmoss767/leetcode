d40a3f7195ef1ad121b577fdc42babd43f74f6e4
Chris Moss <hcmoss70@gmail.com>
2022-06-26 12:57:45 +0200

    made changes

--- medium/findMinInRotatedSortedArray.js
+++ medium/findMinInRotatedSortedArray.js
@@ -3,33 +3,22 @@
  * @return {number}
  */
  var findMin = function(nums) {
-    let left = 0
-    let right = nums.length-1
-    
-    if(nums.length==1){
-        return nums[0]
+    if (nums.length === 1) return nums[0];
+  
+    let left = 0;
+    let right = nums.length - 1;
+  
+    if (nums[left] < nums[right]) return nums[left];
+  
+    while (left < right) {
+      const mid = Math.floor((left + right) / 2);
+      if (nums[mid] > nums[mid + 1]) return nums[mid + 1];
+      if (nums[mid - 1] > nums[mid]) return nums[mid];
+  
+      if (nums[mid] > nums[left]) {
+        left = mid;
+      } else {
+        right = mid;
+      }
     }
-    if(nums[left]<nums[right]){
-        return nums[left]
-    }
-    while(left<=right){
-        const mid = Math.floor(left+right/2)
-        
-        const leftVal = nums[left]
-        const midVal = nums[mid]
-        const leftOfMid = nums[mid-1]
-        const rightOfMid = nums[mid+1]
-        
-        if(midVal> rightOfMid){
-            return rightOfMid
-        }else if(leftOfMid> midVal){
-            return midVal
-        }
-        if(midVal>leftVal){
-            left = mid+1
-        }else{
-            right = mid-1
-        }
-    }
-    
-};
+  };
