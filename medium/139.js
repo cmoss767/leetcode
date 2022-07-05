e53cda0e288c8a0bfd3886e4153efcc84a32cd32
Chris Moss <hcmoss70@gmail.com>
2022-07-05 09:18:19 -0400

    new commit

---
+++ medium/139.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+ var wordBreak = function(s, wordDict) {
+    const table = new Array(s.length+1).fill(false)
+    
+    table[0]=true
+    
+    for(let i = 0;i<table.length;i++){
+        if(table[i]===false) continue
+        for(let j = i +1; j<table.length;j++){
+            if(wordDict.includes(s.slice(i,j))){
+                table[j]=true
+            }
+        }
+    }
+    return table[table.length-1]
+};
