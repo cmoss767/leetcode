bedf2354094a22c97b0b4a8a35563570ef57287e
Chris Moss <hcmoss70@gmail.com>
2022-07-19 20:35:27 -0400

    new commit

---
+++ hard/51.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+
+ const formatBoard = board =>{
+    let result = []
+    
+    for(let col = 0; col < board.length;col++){
+        let newRow = new Array(board.length).fill('.')
+        newRow[board[col]] = 'Q'
+        result.push(newRow.join(''))
+    }
+    return result
+}
+
+
+var solveNQueens = function(n) {
+    const result = []
+    
+    const dfs = (i, n, slate) =>{
+        let lastQ = i - 1
+        
+        for(let prevQ = 0; prevQ < lastQ; prevQ++){
+            if(slate[prevQ] === slate[lastQ]) return
+            
+            let rowDiff = Math.abs(prevQ - lastQ)
+            let colDiff = Math.abs(slate[prevQ] - slate[lastQ])
+            
+            if(rowDiff === colDiff) return
+        }
+        
+        if(i === n){
+            result.push(slate.slice())
+            return
+        }
+        
+        for(let col = 0; col < n; col++){
+            slate.push(col)
+            dfs(i + 1, n, slate)
+            slate.pop()
+        }
+    }
+    
+    dfs(0, n, [])
+    return result.map(board => formatBoard(board))
+};
